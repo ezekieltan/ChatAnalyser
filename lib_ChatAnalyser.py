3797e315caf7fcbc0904c12a27b69bcbe710e432
ezekieltan <39935586+ezekieltan@users.noreply.github.com>
2023-08-30 14:43:43 +0800

    bug fixes and sticker counting

--- lib_ChatAnalyser.py
+++ lib_ChatAnalyser.py
@@ -3,7 +3,7 @@ import chatReader
 import re
 import datetime
 import string
-import json
+
 class ChatAnalyser:
     def __init__(self, paths):
         messages = []
@@ -21,12 +21,12 @@ class ChatAnalyser:
 
         count = lambda l1,l2: sum([1 for x in l1 if x in l2])
         self.df['punctuation'] = self.df['text'].apply(lambda s: count(s, string.punctuation))
+        self.df['sticker'] = self.df['type'].apply(lambda s: s == 'sticker')
 
         tempDateWorker = self.df["dateTime"] - datetime.timedelta(hours=6)
         self.df['date'] = tempDateWorker.dt.floor('d')
 
         self.df['timeDiff'] = self.df['dateTime'].diff()
-        #df.drop('text',axis=1).drop('from',axis=1)
 
         swaps = {}
         count = 0
@@ -37,7 +37,6 @@ class ChatAnalyser:
                 swaps[currentDate] = -1
             if(count>1 and prevFrom !=currentFrom):
                 swaps[currentDate] = swaps[currentDate] + 1
-            prevDateTime = currentDate
             prevFrom = currentFrom
             count = count + 1
             
@@ -48,7 +47,7 @@ class ChatAnalyser:
         self.dateSummary['swapsPerCharacter'] = self.dateSummary['swaps']/self.dateSummary['length']
         self.chatSpan = (pd.to_datetime(min(self.df['date'])).date(),pd.to_datetime(max(self.df['date'])).date())
     def getBasicStats(self):
-        return self.df.drop('date', axis = 1).groupby(['from']).sum()
+        return self.df.drop('date', axis = 1).groupby(['from']).sum()[['length', 'caps', 'count', 'emoji', 'punctuation', 'sticker']]
     def getCharRatio(self):
         basicStats = self.getBasicStats()
         return basicStats['length'][0]/basicStats['length'][1]
@@ -62,13 +61,10 @@ class ChatAnalyser:
         one = dfDateFrom[dfDateFrom['from']==self.parties[0]].set_index(['date'])
         two = dfDateFrom[dfDateFrom['from']==self.parties[1]].set_index(['date'])
         joined = one.join(two, lsuffix='_one', rsuffix='_two', how='outer')[['length_one', 'length_two']].fillna(0)
-        #joined = joined.reindex(pd.date_range(chatSpan[0], chatSpan[1]), fill_value=0)
         joined.index.names = ['date']
         joinedRolling = joined.rolling(days).sum()
         joinedRolling['ratio'] = joinedRolling['length_one']/joinedRolling['length_two']
         return joinedRolling.reset_index()
-    def getLongestGaps(self):
-        return self.df.sort_values('dateTime').dateTime.diff().sort_values().to_frame().join(self.df,lsuffix='_gap')[['dateTime_gap','dateTime', 'from','text']]
     def getDf(self):
         return self.df
         
